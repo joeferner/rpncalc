07e69a598ea321c9306dd29b46c14cd72b3b29a1
Joe Ferner <joe@fernsroth.com>
2015-05-31 08:22:50 -0400

    fixes for arch

--- web/public/scripts/browserifyEntry.js
+++ web/public/scripts/browserifyEntry.js
@@ -1,9 +1,9 @@
 'use strict';
 
-exports.rpncalc = require('../../lib/rpncalc');
-exports.stackItem = require('../../lib/stackItem');
-exports.expressionEvaluator = require('../../lib/expressionEvaluator');
-exports.keys = require('../../lib/keys');
+exports.rpncalc = require('../../../lib/rpncalc');
+exports.stackItem = require('../../../lib/stackItem');
+exports.expressionEvaluator = require('../../../lib/expressionEvaluator');
+exports.keys = require('../../../lib/keys');
 exports.sf = require('sf');
 
 document.require = require;

e8ef5e47d756d270341d7fd8508fe85728a369e6
Joe Ferner <joe@fernsroth.com>
2016-04-16 15:09:59 -0400

    start converting to react

--- web/public/scripts/browserifyEntry.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.rpncalc = require('../../../lib/rpncalc');
-exports.stackItem = require('../../../lib/stackItem');
-exports.expressionEvaluator = require('../../../lib/expressionEvaluator');
-exports.keys = require('../../../lib/keys');
-exports.sf = require('sf');
-
-document.require = require;
